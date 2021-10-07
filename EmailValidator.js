5bb7a4088f6c3c848a1b950ca79f2f323f3aeff0
Akash Singh <akashsingh6280@gmail.com>
2021-10-08 02:43:01 +0530

    MODIFIED: function to validate second mandatory part

--- EmailValidator.js
+++ EmailValidator.js
@@ -1,17 +1,21 @@
 function emailValidator(email){
-    let emailRegex = RegExp("[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9]");
+    let emailRegex = RegExp("^[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9][@][a-zA-Z0-9]+");
     if(emailRegex.test(email))
         console.log("Valid");
     else 
         throw 'Email is not valid';
 }
 
-let emailArray = new Array();
-emailArray.push("abc");
-emailArray.forEach(element => {
-    try{
-        emailValidator(element);
-    }catch(e){
-        console.error(e);
-    }
-});
+
+try{
+    emailValidator("abc@bridgelabz");
+}catch(e){
+    console.error(e);
+}
+
+
+try{
+    emailValidator("bridgelabz");
+}catch(e){
+    console.error(e);
+}
